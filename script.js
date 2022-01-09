a90df622d762a44f40ccedf464b203ed2032036a
Jorge Mendez <mendezjorge98@gmail.com>
2022-01-09 14:38:58 -0300

    Agrego practica 3, cambiar color de una cuadrado V2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//Practica 1
 function cambiarFondo() {
     var text = document.getElementById('input');
     var cuadro = document.getElementById('cuadro');
@@ -9,7 +9,7 @@ function cambiarFondoCuadro(color) {
     var cuadro = document.getElementById('cuadro');
     cuadro.setAttribute('style', `background-color: ${color} ;`);
 }
-
+//Practica 2
 function calcular(operacion) {
     let operando1 = document.calculadora.operando1.value;
     let operando2 = document.calculadora.operando2.value;
@@ -19,5 +19,15 @@ function calcular(operacion) {
         let resultado = eval(operando1 + operacion + operando2);
         document.getElementById('calculadora-resultado').innerText = resultado;
     }
-
+}
+//Practica 3
+function cambiarColor(){
+    var i=0;
+    for(i; i<document.cambiarColorForm.colores.length;i++){
+        if(document.cambiarColorForm.colores[i].checked){
+            break;
+        }
+    }
+    document.getElementById('cuadro2').setAttribute(`style`,`background-color:${document.cambiarColorForm.colores[i].value};`)
+    
 }
