5f7912fa9d47d97c4c00583f5af5241427c7c767
Jorge Mendez <mendezjorge98@gmail.com>
2022-01-09 19:56:35 -0300

    Limpio archivo html de etiquetas js(eventos quedan en el archivo js)

--- script.js
+++ script.js
@@ -1,33 +1,63 @@
 //Practica 1
-function cambiarFondo() {
+document.getElementById('btnCambiarFondo').addEventListener(`click`, function cambiarFondo() {
     var text = document.getElementById('input');
     var cuadro = document.getElementById('cuadro');
     cuadro.setAttribute('style', `background-color: ${text.value} ;`);
 
-}
-function cambiarFondoCuadro(color) {
-    var cuadro = document.getElementById('cuadro');
-    cuadro.setAttribute('style', `background-color: ${color} ;`);
-}
+});
+document.getElementById('btnCambiarFondoRojo').addEventListener(`click`, function cambiarARojo() {
+    document.getElementById('cuadro').setAttribute('style', `background-color: red ;`);
+});
+document.getElementById('btnCambiarFondoVerde').addEventListener(`click`, function cambiarAVerde() {
+    document.getElementById('cuadro').setAttribute('style', `background-color: green ;`);
+});
+document.getElementById('btnCambiarFondoAmarillo').addEventListener(`click`, function cambiarAAmarillo() {
+    document.getElementById('cuadro').setAttribute('style', `background-color: yellow ;`);
+});
+document.getElementById('btnCambiarFondoAzul').addEventListener(`click`, function cambiarAAzul() {
+    document.getElementById('cuadro').setAttribute('style', `background-color: blue ;`);
+});
+
 //Practica 2
-function calcular(operacion) {
+
+document.getElementById('btnSumar').addEventListener('click', function Sumar() {
     let operando1 = document.calculadora.operando1.value;
     let operando2 = document.calculadora.operando2.value;
-    if (operacion == '/' && operando2 == 0) {
+    let resultado = eval(operando1 +'+'+ operando2);
+    document.getElementById('calculadora-resultado').innerText = resultado;
+});
+document.getElementById('btnRestar').addEventListener('click', function Restar() {
+    let operando1 = document.calculadora.operando1.value;
+    let operando2 = document.calculadora.operando2.value;
+    let resultado = eval(operando1 +'-'+ operando2);
+    document.getElementById('calculadora-resultado').innerText = resultado;
+});
+document.getElementById('btnMultiplicar').addEventListener('click', function Multiplicar() {
+    let operando1 = document.calculadora.operando1.value;
+    let operando2 = document.calculadora.operando2.value;
+    let resultado = eval(operando1 +'*'+ operando2);
+    document.getElementById('calculadora-resultado').innerText = resultado;
+});
+document.getElementById('btnDividir').addEventListener('click', function Dividir() {
+    let operando1 = document.calculadora.operando1.value;
+    let operando2 = document.calculadora.operando2.value;
+    if (operando2 == 0) {
         window.alert("No se puede dividir por cero!");
     } else {
-        let resultado = eval(operando1 + operacion + operando2);
+        let resultado = eval(operando1 + '/' + operando2);
         document.getElementById('calculadora-resultado').innerText = resultado;
     }
-}
+});
+
 //Practica 3
-function cambiarColor(){
-    var i=0;
-    for(i; i<document.cambiarColorForm.colores.length;i++){
-        if(document.cambiarColorForm.colores[i].checked){
+document.getElementById('btnCambiarFondoChx').addEventListener('click',function cambiarColor() {
+    var i = 0;
+    for (i; i < document.cambiarColorForm.colores.length; i++) {
+        if (document.cambiarColorForm.colores[i].checked) {
             break;
         }
     }
-    document.getElementById('cuadro2').setAttribute(`style`,`background-color:${document.cambiarColorForm.colores[i].value};`)
-    
-}
+    document.getElementById('cuadro2').setAttribute(`style`, `background-color:${document.cambiarColorForm.colores[i].value};`)
+
+});
+
